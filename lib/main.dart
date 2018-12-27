6d9c89c8b627163e987189e84040e39e75b497e7
wenzhi <1565260753@qq.com>
2018-12-27 14:46:24 +0800

    refact: separate file with pacakge

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_learn/fadeInAndOut/CustomDrawer.dart';
-import 'package:flutter_app_learn/fadeInAndOut/CustomSnackBar.dart';
-import 'package:flutter_app_learn/fadeInAndOut/FadeInAndOut.dart';
+import 'package:flutter_app_learn/animation/FadeInAndOut.dart';
+import 'package:flutter_app_learn/drawer/CustomDrawer.dart';
+import 'package:flutter_app_learn/snackBar/CustomSnackBar.dart';
 
 void main(){
-//  runApp(FadeInAndOut());//淡入淡出效果
-//  runApp(CustomDrawer());
+  runApp(FadeInAndOut());//淡入淡出效果
+  runApp(CustomDrawer());
   runApp(CustomSnackBar());
 }
